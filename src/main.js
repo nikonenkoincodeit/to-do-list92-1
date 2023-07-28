f7aac152ae64cc6f70d65a77620c4a074e039acf
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-28 20:17:13 +0300

    added function onSubmit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
+import { formEl } from './refs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const inputValue = event.target.elements.message.value.trim();
+  if (!inputValue) {
+    return;
+  }
+  event.target.reset();
+}
