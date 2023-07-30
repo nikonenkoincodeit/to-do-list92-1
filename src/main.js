4a40f6020ad40e3f46ee954fe9e9eeb1bcb0fec5
Tosha177777 <slavkoanton4@gmail.com>
2023-07-30 14:39:05 +0300

    form submit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+  const value = e.target.elements.message.value.trim();
+  if (!value) {
+    return;
+  }
+  console.log(value);
+  e.currentTarget.reset();
+}
