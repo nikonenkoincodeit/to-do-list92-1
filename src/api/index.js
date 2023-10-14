a272d4dc530eda810b7deed90c3cbfbd1a69f32e
Balinskiy <3879342@gmail.com>
2023-10-14 16:05:37 +0300

    add

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,16 @@
 const STORAGE_KEY = "to-do-list";
+
+export function saveStorage(data) {
+    const items = getDataFromStorage();
+    items.push(data);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+}
+
+function getDataFromStorage() {
+    try {
+           const data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+    } catch (error) {
+        console.log(error.message);
+    } 
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { formEl } from "./refs";
+import { saveStorage } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
@@ -8,4 +9,9 @@ function onSubmit(event) {
   const value = event.target.elements.message.value.trim();
   console.log(value);
   event.target.reset();
+  saveStorage(createDataObj(value));
+}
+
+function createDataObj(value) {
+  return { value, id: Date.now(), checket: false };  
 }
