b17d6d1604cc61847ab02ff616c47e7faf3583db
Roman-Lviv <kituraroman@gmail.com>
2023-10-14 15:05:04 +0300

    git add.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  const value = event.target.elements.message.value.trim();
+  console.log(value);
+  event.target.reset();
+}
